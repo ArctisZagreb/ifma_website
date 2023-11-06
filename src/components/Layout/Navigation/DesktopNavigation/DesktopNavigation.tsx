55077bc0f5e897414df581e3d066db47b6de3bb2
tsertic <tsertic5@gmail.com>
2023-11-06 16:22:51 +0100

    added mobile navigation

--- src/components/Layout/Navigation/DesktopNavigation/DesktopNavigation.tsx
+++ src/components/Layout/Navigation/DesktopNavigation/DesktopNavigation.tsx
@@ -2,15 +2,28 @@ import { NAVPATHS } from "@/constants";
 
 import React from "react";
 import { NavigationItem } from "./NavigationItem";
-
+import Link from "next/link";
+import Image from "next/image";
 export const DesktopNavigation = () => {
   return (
-    <nav>
-      <ul className="flex  h-full ">
-        {NAVPATHS.map((navItem) => {
-          return <NavigationItem key={navItem.id} navItem={navItem} />;
-        })}
-      </ul>
-    </nav>
+    <div className="hidden lg:flex container-wide  items-center justify-between">
+      <div>
+        <Link href="/" aria-label="homepage">
+          <Image
+            src="/assets/logos/ifma.avif"
+            width={300}
+            height={75}
+            alt="ifma logo"
+          />
+        </Link>
+      </div>
+      <nav className="">
+        <ul className="flex  h-full ">
+          {NAVPATHS.map((navItem) => {
+            return <NavigationItem key={navItem.id} navItem={navItem} />;
+          })}
+        </ul>
+      </nav>
+    </div>
   );
 };
--- src/components/Layout/Navigation/DesktopNavigation/NavigationSubMenu.tsx
+++ src/components/Layout/Navigation/DesktopNavigation/NavigationSubMenu.tsx
@@ -1,18 +1,25 @@
+"use client";
 import { INavItem } from "@/types/types";
 import Link from "next/link";
 import React from "react";
-
 export const NavigationSubMenu = ({
   subMenuItems,
 }: {
   subMenuItems: INavItem[];
 }) => {
   return (
-    <ul className="hidden group-hover:flex absolute top-[114px] left-0 border-t-2 border-blue-500 w-[300px]  flex-col opacity-0 shadow-md group-hover:opacity-100  duration-300 transition-all">
+    <ul className="hidden group-hover:flex absolute top-[114px] left-0 border-t-2 border-blue-500 w-[300px]  flex-col opacity-0 shadow-md group-hover:opacity-100 ">
       {subMenuItems.map((childrenLink) => {
         return (
-          <li key={childrenLink.id} className="py-[10px] px-[10px]">
-            <Link href={childrenLink.path} aria-label={childrenLink.text}>
+          <li
+            key={childrenLink.id}
+            className="py-[15px] px-[10px] group/item hover:bg-base-200/40 transition-universal  "
+          >
+            <Link
+              href={childrenLink.path}
+              aria-label={childrenLink.text}
+              className="block group-hover/item:translate-y-[-2px] group-hover/item:translate-x-[2px] group-hover/item:text-primary duration-300"
+            >
               {childrenLink.text}
             </Link>
           </li>
